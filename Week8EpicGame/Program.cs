296e2e9ffbe05188164f9487b91bd012fac91781
unknown <Rando.Tarkmeel@tktk.ee git config --global user.name>
2024-11-01 01:37:59 +0200

    add week8 code

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,89 @@
+﻿string folderPath = @"C:\Users\Laptop\OneDrive\Dokumendid\data";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine( folderPath, villainFile));
+
+//string[] heroes = { "Harry Potter", "Luke Skywalker", "Lara Craft", "Scooby-Doo" };
+//string[] villains = { "Voldemort", "Darth Vader", "Dracula", "Joker", "Sauron" };
+
+
+
+string[] weapon = { "magic wand", "plastic fork", "banana", "wooden sword", "lego brick" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroweapon = GetRandomValueFromArray(weapon);
+int heroHP = GetCharacterHP(hero);
+int heroStrikeStrength = heroHP;
+Console.WriteLine($"Today {hero} ({heroHP} HP) wtih {heroweapon} saves the day" );
+
+string villainweapon = GetRandomValueFromArray(weapon);
+string villain = GetRandomValueFromArray(villains);
+int villainHP = GetCharacterHP(villain);
+int villainStrikeStrength = heroHP;
+Console.WriteLine($"Today {villain} ({villainHP} HP) with {villainweapon} tries to take over the world!");
+
+while(heroHP > 0 && villainHP > 0)
+{
+    heroHP = heroHP - Hit(villain, villainStrikeStrength);
+    villainHP = villainHP - Hit(hero, heroStrikeStrength);
+}
+
+Console.WriteLine($"Hero {hero} HP: {heroHP}");
+Console.WriteLine($"Villain {villain} HP: {villainHP}");
+
+if(heroHP > 0)
+{
+    Console.WriteLine($"{hero} saves the day!");
+}
+else if (villainHP > 0)
+{
+    Console.WriteLine($"Dark side wins!");
+}
+else
+{
+    Console.WriteLine("Draw!");
+}
+    
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
+
+static int GetCharacterHP(string characterName)
+{
+    if(characterName.Length < 10)
+    {
+        return 10;
+    }
+    else
+    {
+        return characterName.Length;
+    }
+}
+
+
+static int Hit(string characterName, int characterHP)
+{
+    Random rnd = new Random();
+    int strike = rnd.Next(0, characterHP);
+
+    if(strike == 0)
+    {
+        Console.WriteLine($"{characterName} missed the target!");
+    }
+    else if(strike == characterHP - 1)
+    {
+        Console.WriteLine($"{characterName} made a critical hit!");
+    }
+    else
+    {
+        Console.WriteLine($"{characterName} hit {strike}");
+    }
+
+    return strike;
+}
